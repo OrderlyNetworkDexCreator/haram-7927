2eef46cdf15de85b249c8ffe27268a0f8a249931
Mario Reder <mario.reder@pm.me>
2025-10-09 08:45:58 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "HARAM DEX - Holy Altcoin Risk Acceleration Mechanism",
   "VITE_SEO_SITE_DESCRIPTION": "A Holy Dex",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/haram-7927/",
+  "VITE_SEO_SITE_URL": "https://haramdex.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
